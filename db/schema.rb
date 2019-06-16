cfffb953d2696728b7f77e3496954dd02059a3d2
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-06-16 14:33:53 +0900

    Build Message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_112834) do
+ActiveRecord::Schema.define(version: 2019_06_16_052152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
